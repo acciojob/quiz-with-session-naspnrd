acf210bf43a4633f5c4a2a2f682e0e973ae4e61c
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-07-02 22:09:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function displayScore() {
 		if(userAnswers[index] === question.answer)
 			score++;
 	});
-	scoreElement.textContent = `Your score is ${score} out of ${questions.length}`;
+	scoreElement.textContent = `Your score is ${score} out of ${questions.length}.`;
 	localStorage.setItem("score", score);
 }
 renderQuestions();
